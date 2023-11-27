e21f43e6809d0d0d80eb29da3218baa82ad91d2f
NHN <2274560478@qq.com>
2023-11-27 12:36:28 +0800

    refactor:注释掉kafaka模块以启动项目

--- code/data-service/src/main/java/ldn/cs/decision/kafka/WealthConsumer.java
+++ code/data-service/src/main/java/ldn/cs/decision/kafka/WealthConsumer.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@Service
+//@Service
 public class WealthConsumer {
     @Autowired
     private WealthDecisionDao wealthDecisionDao;
